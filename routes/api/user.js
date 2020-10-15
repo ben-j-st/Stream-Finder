693035ef5ce931fe40238434bb2c415f99f7da7a
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-15 12:23:39 +1100

    added semi colons

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,10 +3,10 @@ const userController = require("../../controllers/userController");
 
 // Matches with "/api/user"
 router.route("/")
-    .post(userController.create)
+    .post(userController.create);
 
 router.route("/login")
-    .post(userController.findOne)
+    .post(userController.findOne);
 
 module.exports = router;
 
