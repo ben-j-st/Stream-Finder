65e3a63420f85b507bd1d3d2058920cae1900409
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-15 12:26:12 +1100

    made email unique in schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     firstName: {type: String},
     lastName: {type: String},
