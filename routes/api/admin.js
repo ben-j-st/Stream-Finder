5cfa72d95b7f72afa51820ecb2cf41901286aad2
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-19 14:15:16 +1100

    added more routes to admin

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,8 +2,16 @@ const router = require("express").Router();
 const adminController = require("../../controllers/adminController")
 
 // Matches with "/api/admin"
-router.route("/")
-  .get(adminController.saveNetflix);
+router.route("/netflix")
+    .get(adminController.saveNetflix);
 
+router.route("/amazon")
+    .get(adminController.saveAmazon);
+
+router.route("/update/amazon")
+    .get(adminController.updateAmazon);
+
+router.route("/test")
+    .post(adminController.testSearch);
 
 module.exports = router;
