af0c3ff669eda2df378fe8bc0f34594096bccfaf
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-19 14:07:59 +1100

    removed unecesary code added some pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import { UserProvider } from "./util/userContext";
 
 // Pages 
 import Main from "./pages/main";
 import SignUp from "./pages/signup";
 import Login from "./pages/login";
-import ResetPassword from "./pages/resetPassword"
+import ResetPassword from "./pages/resetPassword";
+import Admin from "./pages/admin";
 
 // own components
 import Header from "./components/header";
@@ -14,54 +14,26 @@ import Footer from"./components/footer";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
+import { UserProvider } from "./util/userContext";
 
 
 function App() {
 
-    const [search, setSearch] = React.useState([])
-    
-    const [user, setUser] = React.useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        isLoggedOn: true
-    })
-
-
     return (
         <Router>
             <UserProvider>
-                <Header 
-                search={search}
-                setSearch={setSearch}
-                user={user}
-                setUser={setUser}
-                />
+                <Header />
                 <Switch>
+                    <Route path="/admin" component={Admin} />
                     {/* signup Page for new users to access more features */}
-                    <Route path="/signup">
-                        <SignUp 
-                        // user={user}
-                        // setUser={setUser}
-                        />
-                    </Route>
+                    <Route path="/signup" component={SignUp}/>
                     {/* login page for returning users */}
-                    <Route path="/login">
-                        <Login 
-                        // user={user}
-                        // setUserLogin={setUser}
-                        />
-                    </Route>
+                    <Route path="/login" component={Login} />
                     {/* if user forgets password */}
-                    <Route path="/resetpassword">
-                        <ResetPassword />
-                    </Route>
+                    <Route path="/resetpassword" component={ResetPassword} />
                     {/* main page that loads first display basic data */}
-                    <Route path="/">
-                        <Main 
-                            search={search}
-                        />
-                    </Route>
+                    <Route path="/" component={Main} />
+                    
                 </Switch>
             </UserProvider>
             <Footer />
