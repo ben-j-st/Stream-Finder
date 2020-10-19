e11710722c11d06181463081c52456eb49c3d21c
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-19 14:14:16 +1100

    added call to retrieve netflix data

--- controllers/netflixController.js
+++ controllers/netflixController.js
@@ -1,13 +1,13 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the netflixController
 
 module.exports = {
     findAll: function(req, res) {
         db.Netflix
-          .find(req.query)
-          .then(dbModel => res.json(dbModel))
-          .catch(err => res.status(422).json(err));
+            .find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     },
 }
 
