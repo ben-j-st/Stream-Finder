714a69d5486b6c2489b43ddab7eaa979fef2670f
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-19 14:09:28 +1100

    added buttons to horizon scroll with animation

--- client/src/components/LineGrid/index.js
+++ client/src/components/LineGrid/index.js
@@ -3,31 +3,96 @@ import GridList from '@material-ui/core/GridList';
 import { makeStyles } from '@material-ui/core/styles';
 import "./style.css"
 
+import Grid from "@material-ui/core/Grid"
+import Hidden from "@material-ui/core/Hidden";
+
+
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
+        maxHeight: "237px"
     },
     gridList: {
-      flexWrap: 'nowrap',
-      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-      transform: 'translateZ(0)',
+        flexWrap: 'nowrap',
+        overflow: 'auto',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+     
+    },
+    centerIcon: {
+        display: "flex",
+        margin: "auto",
+        cursor: "pointer",
+        '&:hover': {
+            background: "#f00",
+        },
     }
 }));
 
 function LineGrid({children}) {
     const classes = useStyles();
+    const navRef = React.useRef();
+    const maxAmount = 700
+    const intervalTiming = 50
+
+
+    function handleNav(direction) {
+        let scrollAmount = 0;
+        if (direction === "left") {
+            const slideTimer = setInterval(() => {
+                navRef.current.scrollLeft -= 100;
+                scrollAmount += 100;
+                if(scrollAmount >= maxAmount){
+                    clearInterval(slideTimer);
+                }
+            }, intervalTiming);
+            
+        } else {
+            const slideTimer = setInterval(() => {
+                navRef.current.scrollLeft += 100;
+                scrollAmount += 100;
+                if(scrollAmount >= maxAmount){
+                    clearInterval(slideTimer);
+                }
+            }, intervalTiming);
+        }
+    }
+
+                
 
     return (
         <div className={classes.root}>
-            <GridList className={classes.gridList} cols={2.5}>
-                {children}
-            </GridList>
+            <Grid container justify="center" alignItems="center" style={{
+                maxHeight:"237px"
+            }}>
+                <Grid item xs={false} sm={1}>
+                    <Hidden only="xs">
+                            <NavigateBeforeIcon  
+                                className={classes.centerIcon}
+                                onClick={() => handleNav("left")}
+                            />
+                    </Hidden>
+                </Grid>
+                <Grid item xs={12} sm ={10}>
+                    <GridList className={classes.gridList} ref={navRef}>
+                        {children}
+                    </GridList>
+                </Grid>
+                <Grid item xs={false} sm={1}>
+                    <Hidden only="xs">
+                        <NavigateNextIcon  
+                            className={classes.centerIcon}
+                            onClick={() => handleNav("right")}
+                        />
+                    </Hidden>
+                </Grid>
+            </Grid>
+            
+            
+           
         </div>
     )
 }
