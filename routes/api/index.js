3d4e9e48705b077fd495142ef6055c455c82dc30
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-19 14:15:57 +1100

    updated routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 const netflixRoutes = require("./netflix");
 const adminRoutes = require("./admin");
 const userRoutes = require("./user");
+const amazonRouter = require("./amazon");
+const searchRoutes = require("./search");
 
 // Book routes
 router.use("/netflix", netflixRoutes);
 router.use("/admin", adminRoutes);
 router.use("/user", userRoutes);
+router.use("/amazon", amazonRouter);
+router.use("/search", searchRoutes);
 
 
 module.exports = router;
