8880d894f9d11683b78f7014ea1293d939baf2fa
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-24 21:29:03 +1100

    created search page and functions

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-
+import SearchCard from "../components/searchCard";
+import Grid from "@material-ui/core/Grid"
 
 import { UserContext } from "../util/userContext"
 import { useHistory } from "react-router-dom";
@@ -9,6 +10,7 @@ function SearchPage() {
     const history = useHistory();
     const {user} = React.useContext(UserContext)
     const isLoggedOn = user.isLoggedOn;
+    const searchData = user.searchData;
 
     useEffect(() => {
         onLoad()
@@ -17,8 +19,6 @@ function SearchPage() {
     function onLoad(){
         if (isLoggedOn === "user") {
             console.log("user is authenticated")
-
-           
         } else {
             console.log("not logged in")
             history.push("/")
@@ -27,7 +27,27 @@ function SearchPage() {
 
     return (
         <>
-            
+            <h1>Search Page </h1>
+            <Grid container justify="center" spacing={1}>
+                {searchData.length ? (
+                    searchData.map(element => (
+                       
+                            <SearchCard
+                            key={element.data.imdb_id} 
+                            title={element.data.name}
+                            image={element.data.offers[0].image}
+                            description={element.data.desc}
+                            offers={element.data.offers}
+                            />
+                        
+                    ))
+                ) : (
+                    <h1>Failed to get results</h1>
+                )}
+                <div className="spaceBuffer" style={{
+                marginBottom: "500px"
+            }}/>
+            </Grid>
         </>
     )
 }
