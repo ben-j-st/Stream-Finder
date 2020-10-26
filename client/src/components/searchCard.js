5b42a57877e4c2742267dd34e252aa76c1b7acf5
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-26 12:31:37 +1100

    updated to use logos and a switch render

--- client/src/components/searchCard.js
+++ client/src/components/searchCard.js
@@ -6,41 +6,31 @@ import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid"
 import Button from '@material-ui/core/Button'
 
+import DisneyLogo from "../logo/disney.png"
+import NetflixLogo from "../logo/netflix.png";
+import PrimeLogo from "../logo/prime.png";
+import HuluLogo from "../logo/hulu.jpg";
+
 const useStyles = makeStyles({
-  root: {
-    background: "rgb(255,255,255,0.6)",
-  },
-  media: {
-    height: 150,
-    width: "auto"
-  },
+    root: {
+        background: "rgb(255,255,255,0.6)",
+    },
+    media: {
+        height: "150px",
+        width: "auto"
+    },
+    logo: {
+        height: "50px",
+        width: "auto",
+        textDecoration: "none",
+        marginRight: " 15px"
+    }
 });
 
     
 export default function MediaCard(props) {
     const classes = useStyles();
     let provider = props.offers
-    // let providerArray = []
-    // let filteredProviders = []
-
-    // function getProviders() {
-    //     console.log("function started")
-    //     filteredProviders = provider.filter(item_el => {
-    //         console.log(item_el)
-    //         return providerArray.filter(filtered_el => {
-    //             if(item_el.provider !== filtered_el) {
-    //                 console.log("item el \n")
-    //                 console.log(item_el)
-    //                 console.log("filtered el \n")
-    //                 console.log(filtered_el)
-    //                 providerArray.push(item_el.provider)
-    //                 return filtered_el.provider == item_el
-    //             }
-    //         }).length == 0
-    //     })
-    // }
-
-    // getProviders()
 
     return (
         <Grid item xs={12} sm={6} md={12}>
@@ -63,9 +53,35 @@ export default function MediaCard(props) {
                     {provider.length ? (
                         provider.map(item => {
                             if(item.flat === 1) {
-                                return (
-                                <Button target="_blank" href={item.url}>{item.provider}</Button>
-                                )
+                                switch (item.provider) {
+                                    case "disneyplus":
+                                        
+                                        return (
+                                            <a target="_blank" rel="noopener noreferrer"  href={item.url}><img src={DisneyLogo} alt={item.provider} className={classes.logo} /></a>
+                                        )
+                                    case "hulu":
+                                    
+                                        return (
+                                            <a target="_blank" rel="noopener noreferrer" href={item.url}><img src={HuluLogo} alt={item.provider} className={classes.logo} /></a>
+                                        )
+                                    case "primevideo":
+                                    
+                                        return (
+                                            <a target="_blank" rel="noopener noreferrer" href={item.url}><img src={PrimeLogo} alt={item.provider} className={classes.logo} /></a>
+                                        )
+
+                                    case "netflix":
+                                    
+                                        return (
+                                            <a target="_blank" rel="noopener noreferrer" href={item.url}><img src={NetflixLogo} alt={item.provider} className={classes.logo} /></a>
+                                        )
+                            
+                                    default:
+                                        return (
+                                            <Button target="_blank" rel="noopener noreferrer" href={item.url}>{item.provider}</Button>
+                                            )
+                                }
+                                
                             }
                         })
                     ): (
