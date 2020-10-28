0a546cb2da86ba8656af0b387e9d761b63ea2394
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-28 18:02:55 +1100

    updated to include modal

--- client/src/components/gridImage.js
+++ client/src/components/gridImage.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import GridListTile from '@material-ui/core/GridListTile';
+import Modal from "./modal"
 
 function GridImage(props) {
-
+    const char = "&#39;"
+    const string = toString(props.title)
+    function onClick() {
+        setShow(true)
+        console.log(char.normalize())
+    }
+    function handleClose() {
+        setShow(false)
+    }
+    const [show, setShow] = React.useState(false)
     return (
         <GridListTile key={props.key}>
-            <img src={props.img} alt={props.title} style={{
-                maxHeight: "237px"
-            }}/>
+            <img src={props.img} alt={props.title} style={{maxHeight: "237px"}} onClick={onClick}/>
+            {show? (
+                <Modal 
+                show={show}
+                img={props.img}
+                setShow={setShow}
+                handleClose={handleClose}
+                title={props.title}
+                description={props.description}
+                char={char}
+                string={string}
+            />
+            ): (
+                <>
+                </>
+            )}
+            
         </GridListTile>
     )
         
