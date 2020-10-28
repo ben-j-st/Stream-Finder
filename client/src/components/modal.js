dc30f348a66fe88b35d22986cfe8c897de7440b3
Ben Stephens <ben_j_stephens@hotmail.com>
2020-10-28 18:02:32 +1100

    got modal working on click

---
+++ client/src/components/modal.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: "black"
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function TransitionsModal(props) {
+  const classes = useStyles();
+
+  const { show, handleClose, title, description, img, string } = props
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={show}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={show}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">{title}</h2>
+            <p id="transition-modal-description">{description}</p>
+            <img src={img} alt={title} />
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
